1e90a357fab5a81cb0d85548f6eb11e13a87b92a
Vicent Ferrandiz <viferpar@gmail.com>
2022-01-03 19:30:40 +0100

    first commit

---
+++ src/main/java/com/viferpar/app/JavaDddArqHexCqrsEventDrivenArqApplication.java
@@ -0,0 +1,13 @@
+package com.viferpar.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaDddArqHexCqrsEventDrivenArqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaDddArqHexCqrsEventDrivenArqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viferpar/app/JavaDddArqHexCqrsEventDrivenArqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viferpar.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaDddArqHexCqrsEventDrivenArqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
